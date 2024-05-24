600db9c58175bd0b374745205fdc49d4f8b4bcb7
CARLOS PEREZ CALDERON <L19091404@zacatepec.tecnm.mx>
2024-05-23 22:51:11 -0600

    X

--- TSO-Proyect/Program.cs
+++ TSO-Proyect/Program.cs
@@ -16,7 +16,7 @@ namespace TSO_Proyect
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new InfoA());
+            Application.Run(new Portada());
         }
     }
 }
